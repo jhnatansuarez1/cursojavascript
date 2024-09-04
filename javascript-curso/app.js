af4d4cfbbf0f6969924e2d9709ff40608cba5c87
jhnatansuarez1 <jhonatanguevara24@gmail.com>
2024-09-03 21:34:31 -0500

    Add files via upload

---
+++ javascript-curso/app.js
@@ -0,0 +1,114 @@
+
+// //tipo de datos
+// "hola mundo" //string
+// 'hola mundo de nuevo' //string
+
+// //numeros
+// 1200000
+// 10000.069
+// -4.5
+// //boleanos 
+
+// true / falce 
+
+// //items o array
+// ['jouu jouu', 'jorgisiuspv','elnr brayan pv']
+// [1,2,2,3,4,5,6,78,9,0]
+// [true,falce,true,falce]
+
+// //objetos object
+// 'jhonatan'
+// 100
+// 14
+// true
+
+// {
+//     "username ": 'jhonatan',
+//     "score": 100,
+//     "hours": 14,
+//     "profecional": true,
+// }
+// {
+//     "username ": 'jordi',
+//     "score": 20,
+//     "hours": 40,
+//     "profecional": falce,
+// // }
+// console.log('esto es una cadena')
+// console.log({"username"=omar pv})
+
+// // variables
+// var nameuser = 'pep guardiola'
+// let lastname ='de el city'
+// nameuser ='pep '
+// const pi =3.1416;
+// console.log(lastname)
+
+// //camelcase(comentario no se nota ni da error )
+// let nickname ='pepolas';
+// let numerouno ='jhonatan';
+// let numerodos='suarez';
+// let complete =numerouno + numerodos
+// let resultado =numeropv > numerosg
+// let numeropv =100
+// let numerogv =500
+// let contraseña ='peppv'
+// let result = imput ==contrasmneor 
+
+// if(resultado=== true)
+// {
+//     console.log(login$$$)
+// }
+// else
+// {
+//     console.log('login poder oso')
+// }
+// if (resultado === falce)
+// {
+//     console.log(loginXXX)
+// // }
+// // console.log(resultado)
+// let score =25 ;
+// if (score > 30){
+//     console.log('nesesita mas practicas ')
+
+// }
+// else if (score >15 ){
+//     console.log('esta mejor estoy orgulloso ')
+// }
+
+
+// else {
+//     console.log('nesesita seguir a mero macho')
+// }
+// let targeta ='targeta '
+// switch (targeta ){
+//     case 'targeta'
+//     console.log('su targeta si  es targeta ')
+//     break;
+//     case 'targeta pv'
+//     console.log('nada ')
+//     break;
+
+// // }
+// //bucles
+// for (let i = 0; i < 50; i++) {
+//     console.log(`Hola menor de nuevo aqui  ${i}`);
+// }
+// const usuarios = ['Juan', 'María', 'Carlos', 'Ana'];
+
+// for (const usuario of usuarios) {
+//     console.log(`Hola, ${usuario}`);
+// }
+//funciones 
+// function saludar(nombre)
+// {
+//     console.log('hola'+nombre)
+
+
+// }
+// saludar ('jorgy');
+// saludar ('nanay');
+// saludar ('locuras');
+// saludar ('en el');
+// saludar ('basque');
